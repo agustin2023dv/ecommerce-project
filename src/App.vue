f261282e068b8607b0c241d0b0981f3dbb382773
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-02-20 11:53:38 +0100

    very first commit. It only includes basic vue and tailwind set up

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <HelloWorld msg="Very first commit"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/styles/tailwind.css'
 
 export default {
   name: 'App',
@@ -14,13 +15,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/">Ecommerce</a></li>
+        
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  // Component logic and properties go here
+};
+</script>
